75b38dcb22c43637c87b212973b504d86504e041
Kc-man6 <kcboytimo@gmail.com>
2025-07-18 14:30:02 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello, backend world!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Hello, backend about!");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Hello, backend contact!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
